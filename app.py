595c74e98eeeaa2ff354e8ea454d47cd8ef586c5
Anthonyive <anthonyzhang0812@gmail.com>
2021-03-31 23:32:51 -0700

    update packages

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://DSCI551:Dsci551@cluster0.ydii8.mongodb.net/Walmart?retryWrites=true&w=majority"
-mongo = PyMongo(app)
-
+app.config['MONGO_URI'] = "mongodb+srv://DSCI551:Dsci551@cluster0.ydii8.mongodb.net/Walmart?retryWrites=true&w=majority"
 
 @app.route('/')
 def index():
-    return render_template('index.html', db=mongo.db)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
