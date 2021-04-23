4b183ffa4b4f385a881c58e9ea8a9090546d3f68
Anthonyive <anthonyzhang0812@gmail.com>
2021-04-23 03:33:58 -0700

    Added filtering checkboxes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify, Blueprint
 from flask_pymongo import PyMongo
 from bson.json_util import loads, dumps
-import json, re
+import json, re, ast
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://DSCI551:Dsci551@cluster0.ydii8.mongodb.net/Walmart?retryWrites=true&w=majority"
@@ -21,13 +21,26 @@ def index():
                             }
                         }])
     catCount = [i.get('count') for i in catCount][0]
+
     return render_template('index.html', docCount=docCount, catCount=catCount)
 
-@app.route('/livesearch', methods=["POST", "GET"])
-def live_search():
+@app.route('/search', methods=["POST", "GET"])
+def search(filter=False):
     query = request.form.get("keyword")
-    result = mongo.db.products.find( { "$text": {"$search": query} } ).limit(100)
+    filterCheckboxes = request.form.get('filterCheckboxes')
+    filterCheckboxes = ast.literal_eval(filterCheckboxes)
+    print(filterCheckboxes)
+    result = mongo.db.products.aggregate(
+        [ 
+            { "$match": { "$text": {"$search": query} }},
+            { "$sort": { "score": { "$meta": "textScore" } } },
+            { "$match": dict.fromkeys(filterCheckboxes, {"$eq": True})},
+            { "$limit": 200}
+        ]
+    )
+    print(dict.fromkeys(filterCheckboxes, {"$eq": True}))
     data = dumps(result)
+        
     return jsonify(items=json.loads(data))
 
 if __name__ == '__main__':
